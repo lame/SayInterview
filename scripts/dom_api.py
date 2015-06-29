ac652d5bf99c2d804e8482e0fe155a8b6604587f
Ryan Kuhl <rkk09c@gmail.com>
2015-06-29 05:42:01 -0400

    DOM API is looking good, only works on python2. Need to move API over to python 2 to keep versioning consistent

--- scripts/dom_api.py
+++ scripts/dom_api.py
@@ -1,4 +1,12 @@
-from lxml import html
+from lxml import etree, html
 
+#  MUST USE PYTHON 2.x, libxml in python3 breaks iteration and tostring methods
 def change_to_block_quote(page):
-    
+    in_file = page
+    parser = etree.HTMLParser()
+    tree = etree.parse(in_file, parser)
+
+    for node in tree.xpath('//div[@contenteditable]//p'):
+        node.tag='blockquote'
+
+    return etree.tostring(tree, pretty_print=False, encoding="utf-8")
